5baf796f8690262baad43287906fe03ee2b20785
nghuukhanh99 <huukhanh99x@gmail.com>
2022-02-17 00:14:23 +0700

    add project

---
+++ FlappyBirdRemake/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float velocity;
+
+    private Rigidbody2D rb;
+
+    SpawnManager spawnManager;
+
+    private float yRange = 4.50f;
+
+    private int score = 0;
+
+    private int highScore;
+
+    public Text nowScore;
+
+    public Text highScored;
+
+    public TextMeshProUGUI scoreText;
+
+    int angle;
+
+    int maxAngle = 20;
+
+    int minAngle = -90;
+
+    AudioSource playerAudio;
+
+    public AudioClip wingSound;
+
+    public AudioClip deadSound;
+
+    public AudioClip deadSound2;
+
+    public AudioClip deadSound3;
+
+    public AudioClip checkPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        score = 0;
+
+        spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
+
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+   
+
+    private void Update()
+    {
+        OnMouseDown();
+        cantMoveOutOfYRange();
+
+        if (spawnManager.isGameActive == true)
+        {
+            scoreText.gameObject.SetActive(true);
+        }
+
+        rotateBird();
+        
+        //updateScore();
+    }
+
+
+    public void OnMouseDown()
+    {
+
+        if (spawnManager.canTouch == true && Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = Vector2.up * velocity;
+                
+                rb.constraints = RigidbodyConstraints2D.None;
+
+                playerAudio.PlayOneShot(wingSound, 0.5f);
+            }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D birdCollision)
+    {
+        if (birdCollision.gameObject.CompareTag("Pipe") || birdCollision.CompareTag("Ground"))
+        {
+            spawnManager.GameOver();
+
+            playerAudio.PlayOneShot(deadSound, 0.5f);
+
+            playerAudio.PlayOneShot(deadSound2, 0.5f);
+            
+            playerAudio.PlayOneShot(deadSound3, 0.5f);
+            
+        }
+
+        if (birdCollision.gameObject.CompareTag("PointTrigger"))
+        {
+            score++;
+
+            scoreText.text = score.ToString();
+
+            nowScore.text = score.ToString();
+
+            playerAudio.PlayOneShot(checkPoint, 0.5f);
+        }
+    }
+
+   
+
+    private void cantMoveOutOfYRange()
+    {
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+    }
+
+
+    private void rotateBird()
+    {
+        if(rb.velocity.y > 0)
+        {
+            if(angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if(rb.velocity.y < -1.2)
+        {
+            if (angle >= minAngle)
+            {
+                angle = angle - 3;
+            }
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+
+
+
+    public void updateScore()
+    {
+
+        if (score > highScore)
+        {
+            highScore = score;
+        }
+
+        highScored.text = "" + score;
+
+        if (score < highScore)
+        {
+            nowScore.text = score.ToString();
+        }
+    }
+}
---
+++ FlappyBirdRemake/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SpawnManager : MonoBehaviour
+{
+    PlayerCtrl playCtrl;
+
+    public GameObject[] obstaclesPrefabs;
+
+    private float spawnPosX = -1.7f;
+
+    private float spawnPosMinY = -1.20f;
+
+    private float spawnPosMaxY = 2.2f;
+
+    private float spawnPosZ = 0.2f;
+
+    private float startDelay = 2;
+
+    private float repeatRate = 1.5f;
+
+    private float spawnRate = 1.5f;
+
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive = false;
+
+    public Button restartButton;
+
+    public Button startButton;
+
+    public Button scoreButton;
+
+    public GameObject titleScreen;
+
+    public bool canTouch = false;
+
+    public GameObject tapTitle;
+
+    public GameObject highScoreBoard;
+
+    AudioSource UiAudio;
+
+    public AudioClip restartAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = false;
+
+        canTouch = false;
+
+        highScoreBoard.gameObject.SetActive(false);
+
+        InvokeRepeating("SpawnObstacles", startDelay, repeatRate);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObstacles() // sinh ra pipe
+    {
+        isGameActive = true;
+
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int pipeIndex = Random.Range(0, obstaclesPrefabs.Length);
+
+            Vector3 spawnPos = new Vector3(spawnPosX, Random.Range(spawnPosMinY, spawnPosMaxY), spawnPosZ);
+
+            Instantiate(obstaclesPrefabs[pipeIndex], spawnPos, obstaclesPrefabs[pipeIndex].transform.rotation);
+        }
+        
+    }
+    
+    public void StartGame() // ham bat dau game
+    {
+        isGameActive = true;
+
+        canTouch = true;
+
+        StartCoroutine(SpawnObstacles());
+
+        titleScreen.gameObject.SetActive(false);
+
+        startButton.gameObject.SetActive(false);
+
+        scoreButton.gameObject.SetActive(false);
+
+        tapTitle.gameObject.SetActive(false);
+
+        highScoreBoard.gameObject.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    public void GameOver() // screen game over
+    {
+        restartButton.gameObject.SetActive(true);
+
+        gameOverText.gameObject.SetActive(true);
+
+        scoreButton.gameObject.SetActive(true);
+
+        isGameActive = false;
+
+        canTouch = false;
+
+        Time.timeScale = 0;
+
+        highScoreBoard.gameObject.SetActive(true);
+
+        // pause everything when object dead
+    }
+
+    public void RestartGame() // nut restart game
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        highScoreBoard.gameObject.SetActive(false);
+    }
+
+    public void seeBestScore()
+    {
+        highScoreBoard.gameObject.SetActive(true);
+    }
+
+    
+}
